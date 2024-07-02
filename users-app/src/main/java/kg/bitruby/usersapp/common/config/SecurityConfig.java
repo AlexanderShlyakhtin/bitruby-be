f77515edb61225ec031def30e5fbf45af6070afd
alexander.shlyakhtin <alexander.shlyakhtin@dedalus.eu>
2024-07-02 12:51:52 +0200

    Stable

--- users-app/src/main/java/kg/bitruby/usersapp/common/config/SecurityConfig.java
+++ users-app/src/main/java/kg/bitruby/usersapp/common/config/SecurityConfig.java
@@ -3,9 +3,9 @@ package kg.bitruby.usersapp.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
@@ -27,8 +27,7 @@ public class SecurityConfig {
             .requestMatchers("/error").permitAll()
             .anyRequest().authenticated()
         )
-        .oauth2ResourceServer(oauth2 -> oauth2
-            .authenticationManagerResolver(new JwtIssuerAuthenticationManagerResolver(authUrl)));
+        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
     http.csrf(AbstractHttpConfigurer::disable);
     http.headers(httpSecurityHeadersConfigurer ->
         httpSecurityHeadersConfigurer
